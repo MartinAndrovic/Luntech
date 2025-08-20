d878041ca0fec12744c77c46d72250ffb86bb8ff
martinandrovic <martin.androvic@gmail.com>
2025-08-20 23:23:29 +0200

    Initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title> Luntech </title>
+    @vite('resources/js/app.js') {{-- or mix if you use Laravel Mix --}}
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
